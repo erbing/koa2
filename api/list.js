f4375ecd68f8b4bcdd1c33e8b6476ef493b5d1b1
zhangge <zhangge@zhongan.com>
2017-09-11 13:39:15 +0800

    add monk

--- api/list.js
+++ api/list.js
@@ -1,1 +1,12 @@
-let api = 'api'
+const monk = require('monk')
+const url = 'localhost:27017/vuejs'
+const db = monk(url)
+
+console.log(db)
+
+db.then(() => {
+    console.log('Connected correctly to server')
+})
+
+// db.createCollection("counters")
+// db.counters.insert({_id:"productid",sequence_value:1})
